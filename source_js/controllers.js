eb88c5399dfecd8b026c7924483597ab8f66de9e
andyvuong <andyv.42@gmail.com>
2016-04-17 20:52:41 -0500

    Add statistics partial

--- source_js/controllers.js
+++ source_js/controllers.js
@@ -3,3 +3,11 @@ var hfControllers = angular.module('hfControllers', []);
 hfControllers.controller('IndexController', ['$scope', 'CommonData'  , function($scope, CommonData) {
   // do stuff
 }]);
+
+
+hfControllers.controller('StatisticsController', ['$scope', 'CommonData'  , function($scope, CommonData) {
+  
+
+
+  
+}]);
