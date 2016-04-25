7431817f6e4e17d849e5c3adf484fdb3cea85780
Alek Festekjian <festekj2@illinois.edu>
2016-04-25 18:03:51 -0500

    fixing misspelling

--- source_js/script.js
+++ source_js/script.js
@@ -1,14 +1,3 @@
 toastr.options.timeOut = 1000;
 toastr.options.extendedTimeOut = 1000;
 Foundation.Keyboard.handleKey = function(){};
-$(document).ready(function() {
-
-    // show when page load
-ca
-    $('#signup').click(function() {
-       // show when the button is clicked
-       toastr.success('Logged in!');
-
-    });
-
-});
